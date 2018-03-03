352d359aa7fd4f412714f2d133c520cacf6e509a
Raymond Hammarling <raymond@hammarlings.se>
2018-03-03 14:24:10 +0100

    Use mit module to generate MIT license

--- vanillajs.sh
+++ vanillajs.sh
@@ -77,27 +77,7 @@ cat > test/.eslintrc.json <<EOF
 }
 EOF
 
-cat > LICENSE <<EOF
-Copyright (c) $(date +%Y) $AUTHOR
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-EOF
+npx mit "$AUTHOR" > LICENSE
 
 # Install node modules
 npm install --save-dev eslint ava npm-run-all
